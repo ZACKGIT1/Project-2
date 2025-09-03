993a20d6fdb0327b8b2377a14ed9a8b94e0cfd5f
zackgit23001 <zackpfannerstill19+git1@gmail.com>
2025-09-03 14:06:30 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is version 13")
+	fmt.Fprintf(w, "this is a sample go application deployed via docker-compose.")
 }
 
 func main() {
